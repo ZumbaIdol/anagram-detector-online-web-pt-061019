4f8e3f77385f90b55c36284fd1b3f892444375fc
David Foote <david.a.foote@gmail.com>
2019-07-04 23:35:17 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,11 +7,6 @@ class Anagram
   end
   
   def match(array)
-  #   array.select do |element|
-  #   @word.split("").sort == element.split("").sort
-  # end
-  # end
-    
     array.select {|element, word| element.split("").sort == @word.split("").sort}
   end
 end
